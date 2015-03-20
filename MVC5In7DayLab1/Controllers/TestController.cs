05a54e1186b0efb7f02b2e89c2ad6e28c7b25aca
huuphat <huuphatnguyen@outlook.com>
2015-03-20 13:44:26 +0700

    First Project

---
+++ MVC5In7DayLab1/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5In7DayLab1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ActionResult demo()
+        {
+            return View("phat");
+        }
+    }
+}
---
+++ MVC5In7DayLab1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5In7DayLab1.MvcApplication" Language="C#" %>
